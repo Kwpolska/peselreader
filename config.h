bbfad954885e6636d9d7624a7b4f31e775f91375
Chris “Kwpolska” Warrick <kwpolska@gmail.com>
2014-07-20 18:36:33 +0200

    change stuff around
    Signed-off-by: Chris “Kwpolska” Warrick <kwpolska@gmail.com>

--- config.h
+++ config.h
@@ -35,7 +35,7 @@
 #define PACKAGE "peselreader"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "kwpolska@kwpolska.tk"
+#define PACKAGE_BUGREPORT "chris@chriswarrick.com"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "peselreader"
